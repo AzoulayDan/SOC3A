171aedc54ce9c617d990bc04baa63b50fcbcc54c
azoulayDan <dan.azoulay@imerir.com>
2017-11-07 11:12:31 +0100

    Ajout de la mise en place du serveur

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 #-*- encoding:utf-8 -*-
-<<<<<<< HEAD
 from flask import Flask
-=======
-from flask import Flask 
->>>>>>> 76fb39f0ccc97dde3e6d91a160c193ffe31efd99
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -14,9 +10,75 @@ CORS(app)
 def index():
 	return 'hello world'
 
+########################
+# Methodes GET
+########################
+@app.route('/admin/connect', methods=['GET'])
+def connect_admin():
+	'''
+	Cette route est appelée lors de la connection de
+	l'administrateur de la solution.
+	'''
+	return 'connexion de admin'
+
+@app.route('/admin/teams', methods=['GET'])
+def get_infos_teams():
+	'''
+	Cette route est appelée lorsque l'administrateur
+	de la solution désire l'ensemble des informations
+	concernant les différentes teams.
+	'''
+	return 'différentes teams'
+
+@app.route('/admin/teams/<team>', methods=['GET'])
+def get_infos_team():
+	'''
+	Cette route est appelée lorsque l'administrateur
+	de la solution désire l'ensemble des informations
+	concernant une team.
+	'''
+	return 'infos a propos de une seule team'
+
+@app.route('/game/connect/', methods=['GET'])
+def connect_player():
+	'''
+	Cette route est appelée lorsqu'un joueur se 
+	connecte à une team en particulier.
+	Le client envoie ainsi au serveur l'équipe qu'il 
+	rejoins, et son nom propre à lui.
+	'''
+	return 'connexion à une team pour le jeu'
+
+@app.route('/game/mission/<team>', methods=['GET'])
+def get_mission(team):
+	'''
+	Cette route est appelée lorsque l'on envoie une
+	au client une mission pour une équipe en
+	particulier.
+	'''
+	return 'envoyer une mission à une équipe en particulier'
+
+
+########################
+# Methodes POST
+########################
+@app.route('/game/team/create_account', methods=['POST'])
+def create_account():
+	'''
+	Cette route est appelée lorsque une équipe crée
+	son compte afin de jouer.
+	'''
+	return 'une equipe nouvelle est crée dans le jeu'
+
+@app.route('/game/validate_photo/<team>', methods=['POST'])
+def validate_picture(team):
+	'''
+	Cette route est appelée lorsque le client vérifie
+	si la photo qu'il a pris a été correcte ou non.
+	A mettre ici l'analyse d'image ou non (reste à voir)
+	'''
+	return 'valider une image ici'
+
 if __name__ == '__main__':
-<<<<<<< HEAD
 	app.run()
-=======
-	app.run()
->>>>>>> 76fb39f0ccc97dde3e6d91a160c193ffe31efd99
+
